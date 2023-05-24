651034fd1320dec05ba71672b1b193501d41e350
Julien Bocage <julien.bocage@gmail.com>
2023-05-24 19:03:26 +0800

    try to solve missing files issue

--- src/new_pyprojects/utils/__init__.py
+++ src/new_pyprojects/utils/__init__.py
@@ -5,5 +5,10 @@ class PackagePaths:
     """Contains useful path for the package"""
 
     ROOT = pathlib.Path(__file__).resolve().parents[3].absolute()
+
     TEST = ROOT / "tests"
-    TEMPLATES = ROOT / "src" / "templates"
+
+    UTILS = pathlib.Path(__file__).resolve().parent.absolute()
+    PACKAGE_ROOT = UTILS.parent
+
+    TEMPLATES = PACKAGE_ROOT / "templates"
